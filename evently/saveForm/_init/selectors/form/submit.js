50bf1b920bad5d1f983c75a1560374830733d6e7
Martin Kirchgessner <martin.kirch@gmail.com>
2010-12-19 22:31:16 +0100

    Now we can save the edited document. The 'about' div has been moved. Integrating the Gritter jQuery plug-in

---
+++ evently/saveForm/_init/selectors/form/submit.js
@@ -0,0 +1,17 @@
+function () {
+	var form = this;
+	var doc = {
+    created_at : new Date(),
+		content : $('#content').html(),
+		_id : $('input[type=text]').val()
+  };
+  $$(this).app.db.saveDoc(doc, {
+    success : function() {
+      $.gritter.add({
+				title: 'Saved!',
+				text: '"' + doc._id +'" has been saved.'
+			});
+    }
+  });
+  return false;
+};
