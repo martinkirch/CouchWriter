7919f5f0e2c8bb80be7d35b70a3c5bf84c4af649
Martin Kirchgessner <martin.kirch@gmail.com>
2011-01-12 23:37:24 +0100

    Adding empty Id checking. 'article' now provides a 'save' event

---
+++ evently/article/save.js
@@ -0,0 +1,26 @@
+function (event, docId) {
+	var doc = {
+    created_at : new Date(),
+		content : $('article').html(),
+		_id : docId,
+		type : 'article'
+  };
+
+	if ($$('article')._rev) {
+		doc._rev = $$('article')._rev;
+	}
+	
+	doc.tags = $.map($('#tagsInput').val().split(','), function(tag) { return tag.trim().toLowerCase(); });
+
+  $$(this).app.db.saveDoc(doc, {
+    success : function(data) {
+			$.gritter.add({
+				title: 'Saved!',
+				text: '"' + data.id +'" has been saved.',
+				time: 3000
+			});
+		
+			$$('article')._rev = data.rev;
+		}
+  });
+}
--- evently/saveForm/_init/selectors/form/submit.js
+++ evently/saveForm/_init/selectors/form/submit.js
@@ -1,28 +1,15 @@
 function (event) {
 	event.preventDefault();
 	
-	var doc = {
-    created_at : new Date(),
-		content : $('article').html(),
-		_id : $('input[type=text]').val(),
-		type : 'article'
-  };
-
-	if ($$('article')._rev) {
-		doc._rev = $$('article')._rev;
+	var idInput = $('input[type=text]',this);
+	var id = idInput.val();
+	
+	if (id.length == 0) {
+		idInput.val("Please enter an ID!");
+		var container = this;
+		setTimeout( function () { idInput.val(''); } , 2000);
+		return;
 	}
 	
-	doc.tags = $.map($('#tagsInput').val().split(','), function(tag) { return tag.trim().toLowerCase(); });
-
-  $$(this).app.db.saveDoc(doc, {
-    success : function(data) {
-			$.gritter.add({
-				title: 'Saved!',
-				text: '"' + data.id +'" has been saved.',
-				time: 3000
-			});
-		
-			$$('article')._rev = data.rev;
-		}
-  });
+	$('article').trigger('save', id);
 };
