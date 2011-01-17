259aab898e9d11816f415d149c43902e24fab74b
Martin Kirchgessner <martin.kirch@gmail.com>
2011-01-18 00:28:25 +0100

    Adding a validation function against (annoying) tag injections.

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -6,6 +6,41 @@ function (newDoc, existingDoc, userCtx) {
 		}
 	}
 	
+	function validateContent(content) {
+		var pos = content.indexOf("<");
+		
+		while (pos != -1) {
+			if (content.charAt(++pos) == '/') {
+				pos++;
+			}
+			
+			// forget paragraphs...
+			if (content.charAt(pos) != 'p') {
+				var tag = content.substr(pos, 3);
+				
+				switch (tag) {
+					case "scr": // script
+					case "!--": // comments
+					case "app": // applet
+					case "bas": // base
+					case "bod": // body
+					case "for": // form
+					case "fra": // frame(set)
+					case "ifr": // iframe
+					case "htm": // html
+					case "lin": // link
+					case "nof": // noframes
+					case "nos": // noscript
+					case "obj": // object
+					case "sty": // style
+						throw({unauthorized : "Unauthorized HTML tags found in document's content."});
+				}
+			}
+			
+			pos = content.indexOf("<", ++pos);
+		}
+	}
+	
 	if (!userCtx) {
 		throw({unauthorized : "You must be logged in to save something."});
 	}
@@ -32,6 +67,7 @@ function (newDoc, existingDoc, userCtx) {
 				};
 			}
 			
+			validateContent(newDoc.content);
 			break;
 		default :
 			throw({forbidden : "Invalid \"type\" field."});
